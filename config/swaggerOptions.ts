042c52de4fd107c25761336157070ea7de5b02d7
Silverio Cruz <scruz34-rrc>
2026-04-05 15:03:36 -0500

    Added swagger configurations.

---
+++ config/swaggerOptions.ts
@@ -0,0 +1,37 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions: swaggerJsdoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Baseball Stats Archive API Documentation",
+            version: "1.0.0",
+            description: "This is the API documentation for the Baseball Stats Archive application.",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/api/v1",
+                description: "Local server",
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ["./src/api/v1/routes/*.ts", "./src/api/v1/validation/*.ts"],
+};
+
+export const generateSwaggerSpec = (): object => {
+    return swaggerJsdoc(swaggerOptions);
+};
