83ec849da0c66f0f02f786e95e5ac7f9541ca6bb
Silverio Cruz <scruz34-rrc>
2026-04-05 15:16:47 -0500

    Updated main app file to use swagger and added documentation script to package.json.

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,15 @@ dotenv.config();
 import teamRoutes from "./api/v1/routes/teamRoutes";
 import playerRoutes from "./api/v1/routes/playerRoutes";
 import statRoutes from "./api/v1/routes/statRoutes";
+import setupSwagger from "../config/swagger";
 
 const app: Express = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+setupSwagger(app);
 
 app.use("/api/v1/teams", teamRoutes);
 app.use("/api/v1/players", playerRoutes);
