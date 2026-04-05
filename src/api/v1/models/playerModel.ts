a0d2728025e4d598446ede4dd5ef8c1c266deef3
Silverio Cruz <scruz34-rrc>
2026-04-05 13:13:13 -0500

    Added players, stats, teams interfaces.

---
+++ src/api/v1/models/playerModel.ts
@@ -0,0 +1,9 @@
+export interface Player {
+    id: string;
+    firstName: string;
+    lastName: string;
+    position: string;
+    teamId: string;
+    debutYear: number;
+    isActive: boolean;
+}
---
+++ src/api/v1/models/teamModel.ts
@@ -0,0 +1,8 @@
+export interface Team {
+    id: string;
+    name: string;
+    city: string;
+    league: string;
+    foundedYear: number;
+    isActive: boolean;
+}
