c995d8c5a07c6ad2299e14d4cccb0cd9b0180802
Silverio Cruz <scruz34-rrc>
2026-04-05 13:04:02 -0500

    Set up file structure and added firebase service account and env file to gitignore.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
+});
+
+export default app;
